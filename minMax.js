6e0e47f92c130aefc8bd75d1689830667b2c414e
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-29 14:34:41 +0300

    minmax

--- minMax.js
+++ minMax.js
@@ -1,22 +1,22 @@
 function miniMaxSum(arr) {
-  // Write your code here
-  let sum = 0;
-  let min = Infinity;
-  let max = -Infinity;
-  for (let i = 0; i < arr.length; i++) {
-    for (let k = 0; k < arr.length; k++) {
-      if (i !== k) {
-        sum += arr[k];
-      }
+    // Write your code here
+    let sum = 0;
+    let min = Infinity;
+    let max = -Infinity;
+    for (let i = 0; i < arr.length; i++) {
+        for (let k = 0; k < arr.length; k++) {
+            if (i !== k) {
+                sum += arr[k];
+            }
+        }
+        if (sum < min) {
+            min = sum;
+        }
+        if (sum > max) {
+            max = sum;
+        }
+        sum = 0;
     }
-    if (sum < min) {
-      min = sum;
-    }
-    if (sum > max) {
-      max = sum;
-    }
-    sum = 0;
-  }
-  console.log(min + " " + max);
+    console.log(min + ' ' + max);
 }
 miniMaxSum([1, 2, 3, 4, 5]);
