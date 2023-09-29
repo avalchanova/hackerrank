8ef40ac478366b2e940d918118a1df667ddae779
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-29 14:34:16 +0300

    lonely integer

--- lonelyInteger.js
+++ lonelyInteger.js
@@ -1,31 +1,31 @@
 function lonelyinteger(a) {
-  let unique = undefined;
-  let stopIndex = a.length - 1;
+    let unique = undefined;
+    let stopIndex = a.length - 1;
 
-  if (a.length < 0) {
-    throw new Error("Invalid array length");
-  }
-  if (a.length === 1) {
-    unique = a[0];
-  }
+    if (a.length < 0) {
+        throw new Error('Invalid array length');
+    }
+    if (a.length === 1) {
+        unique = a[0];
+    }
 
-  for (let i = 0; i <= stopIndex; i++) {
-    for (let k = 0; k <= stopIndex; k++) {
-      if (i !== k) {
-        if (a[i] === a[k]) {
-          unique = undefined;
-          break;
-        } else {
-          unique = a[i];
-          //   console.log(unique);
+    for (let i = 0; i <= stopIndex; i++) {
+        for (let k = 0; k <= stopIndex; k++) {
+            if (i !== k) {
+                if (a[i] === a[k]) {
+                    unique = undefined;
+                    break;
+                } else {
+                    unique = a[i];
+                    //   console.log(unique);
+                }
+            }
         }
-      }
+        if (typeof unique === 'number') {
+            break;
+        }
     }
-    if (typeof unique === "number") {
-      break;
-    }
-  }
-  console.log(unique);
-  return unique;
+    console.log(unique);
+    return unique;
 }
 lonelyinteger([1, 1, 2]);
